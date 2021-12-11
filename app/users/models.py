cd38d6b82d740623f8d3958098517f73eb87e26e
Ali Emre SAVAS <aliemresavas.95@gmail.com>
2021-12-11 13:20:52 +0000

    Profile model created.

--- app/users/models.py
+++ app/users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
+import datetime
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    profile_image = models.ImageField(upload_to = "profiles/", default = "profiles/avatar.svg")
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return f'{self.name} --> {self.email} --> {datetime.datetime.strftime(self.created, "%d-%B-%Y %A")}'
