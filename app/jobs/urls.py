f3739606053144e2687a4e21cfc5dc100957b8b2
Ali Emre SAVAS <aliemresavas.95@gmail.com>
2021-12-11 13:07:39 +0000

    Url app_names added.

---
+++ app/jobs/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from django.urls import path
+from . import apps
+
+app_name = "apps.JobsConfig.name"
+
+urlpatterns = [
+    
+]
---
+++ app/users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import apps
+
+
+app_name = "apps.UsersConfig.name"
+
+urlpatterns = [
+    
+]
