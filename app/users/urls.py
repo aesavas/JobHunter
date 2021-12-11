5aff44179f216c505000e61c405f4cab6480efbd
Ali Emre SAVAS <aliemresavas.95@gmail.com>
2021-12-11 23:21:54 +0000

    Logout view added.

--- app/users/urls.py
+++ app/users/urls.py
@@ -3,6 +3,7 @@ from . import apps
 from .views import (
     sign_up_view,
     login_view,
+    logout_view,
 )
 
 app_name = "apps.UsersConfig.name"
@@ -10,4 +11,5 @@ app_name = "apps.UsersConfig.name"
 urlpatterns = [
     path('sign-up/', sign_up_view, name='sign-up'),
     path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
 ]
--- app/users/views.py
+++ app/users/views.py
@@ -48,3 +48,8 @@ def login_view(request):
         else:
             messages.error(request, 'Username or password is incorrect!')
     return render(request, 'users/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, 'User was logged out successfully.')
+    return redirect('landing:landing-page')
