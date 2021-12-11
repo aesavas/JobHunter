2bb362549826c371a896b3620dfec8e7bf80c090
Ali Emre SAVAS <aliemresavas.95@gmail.com>
2021-12-11 14:10:35 +0000

    Skill model added.

--- app/users/models.py
+++ app/users/models.py
@@ -15,3 +15,15 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.name} --> {self.email} --> {datetime.datetime.strftime(self.created, "%d-%B-%Y %A")}'
+
+
+
+class Skills(models.Model):
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    skill_image = models.ImageField(upload_to = "skills/", default = "skills/skills.png")
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return f'{self.owner.name} --> {self.name}'
